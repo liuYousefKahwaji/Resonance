a18c2962fcf706f5266433fa89fe17283dcb5eda
Yousef Kahwaji <32330551@students.liu.edu.lb>
2026-06-28 13:19:59 +0300

    v1.3.2_2

--- lib/core/audio/audio_service.dart
+++ lib/core/audio/audio_service.dart
@@ -59,7 +59,7 @@ class PlayerHandler extends BaseAudioHandler with QueueHandler, SeekHandler {
           _player.play();
         } else if (currentLoopMode == LoopMode.all) {
           await _player.pause();
-          await _player.seek(Duration.zero);
+          //await _player.seek(Duration.zero); fixes occasional skipping
           await Future.delayed(const Duration(milliseconds: 50));
           await next();
         }
